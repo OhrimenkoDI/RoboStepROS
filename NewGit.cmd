f86899536f113c803c6e9106058ef7ffdf254410
OhrimenkoDI <ohrimenkodi@gmail.com>
2025-04-13 23:19:18 +0300

    Новый проект, пока поднял только USART

---
+++ NewGit.cmd
@@ -0,0 +1,12 @@
+echo "# RoboStepROS" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add GitHub https://github.com/OhrimenkoDI/RoboStepROS.git
+git remote add GitVerse https://gitverse.ru/OhrimenkoDI/RoboStepROS.git
+git remote add GitFlic https://gitflic.ru/project/ohrimenkodi/robostepros.git
+git push -u GitHub main
+git push -u GitVerse main
+git push -u GitFlic main
+pause
